6e6149eab31e035c501eaea5f983d086db3e2f60
Jinksi <ericjinks@gmail.com>
2017-10-01 15:47:53 +1000

    Routing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj87uo5zu00j10145glxr5olm'
@@ -13,8 +14,10 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>
   , document.getElementById('root'))
 registerServiceWorker()
