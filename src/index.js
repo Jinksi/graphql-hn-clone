9fb3e2770f08f35892bbe4b7de2dd5d72e6bd670
Jinksi <ericjinks@gmail.com>
2017-10-01 15:04:27 +1000

    Add Apollo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj87uo5zu00j10145glxr5olm'
+})
+const client = new ApolloClient({
+  networkInterface
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'))
+registerServiceWorker()
